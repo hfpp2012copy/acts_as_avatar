0fe1a00b332af88f04d098114d991fe74917aa38
hfpp2012 <hfpp2012@gmail.com>
2022-03-21 15:39:48 +0800

    fix image tag

--- lib/acts_as_avatar/helper.rb
+++ lib/acts_as_avatar/helper.rb
@@ -9,7 +9,7 @@ module ActsAsAvatar
         name = object.send(name.to_sym)
 
         if ActsAsAvatar.configuration.inline_svg_engine.to_sym == :initial_avatar
-          image_tag InitialAvatar.avatar_data_uri(name.first)
+          image_tag InitialAvatar.avatar_data_uri(name.first), **options
         else
           Initials.svg name, size: size, **options
         end
